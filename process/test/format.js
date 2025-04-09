84065c84eb23739706a73480a011490d192acff0
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2025-04-10 05:16:43 +0530

    auto format selector added based on config.yml for process/test

--- process/test/format.js
+++ process/test/format.js
@@ -1,2 +1,14 @@
-const format = "wasm32-unknown-emscripten4"
-export default format 
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+let format;
+try {
+  const config = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+  format = (config?.target === 32) 
+    ? "wasm32-unknown-emscripten4" 
+    : "wasm64-unknown-emscripten-draft_2024_02_15";
+} catch (e) {
+  format = "wasm64-unknown-emscripten-draft_2024_02_15"; // Default if file/error
+}
+
+export default format;
